c1eb5b3839b652258d806031db050dd09d56c48d
tomas99vogel <tomasvogel.9@gmail.com>
2024-12-02 20:36:23 +0100

    part 2 and cleanup

--- 2/02.py
+++ 2/02.py
@@ -1,29 +1,41 @@
-# The levels are either all increasing or all decreasing.
-# Any two adjacent levels differ by at least one and at most three.
 
-def check_report(line:list[int]) -> int:
-    get_direction = line[0] - line[1]
-    # >1 descending, <1 ascending
-    if get_direction in range(-3,0): 
+def part_one_check(line:list[int]) -> int:
+    get_diff = line[0] - line[1]
+    if get_diff == 0 or get_diff < -3 or get_diff > 3:
+        return 0
+    if get_diff in range(-3,0): 
         for i in range(1,len(line)-1):
             if line[i]-line[i+1] not in range(-3,0):
                 return 0
         return 1
-    elif get_direction in range(1,4):
+    if get_diff in range(1,4):
         for i in range(1,len(line)-1):
             if line[i]-line[i+1] not in range(1,4):
                 return 0
         return 1
-    else:
-        return 0 # not safe
 
-def part_one() -> int:
-    safe_count = sum([check_report(line) for line in lines])
-    return safe_count
+def part_two_check(line:list[int]) -> int:
+    if part_one_check(line):
+        return 1
+    else:
+        for i in range(len(line)):
+            if part_one_check(line[:i]+line[i+1::]):
+                return 1
+    return 0
 
 if __name__ == '__main__':
     with open("input.txt") as f:
         lines = [list(map(int, line.split())) for line in f.readlines()]
 
-    print(part_one())
+    part_one = 0
+    part_two = 0
+
+    for line in lines:
+        part_one += part_one_check(line)
+        part_two += part_two_check(line)
+
+    print(part_one)
+    print(part_two)
+
+  
     
